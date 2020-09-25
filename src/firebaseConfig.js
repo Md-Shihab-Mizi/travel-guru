db11475ff36dff6008e65594e7e3d74a1f039362
Md-Shihab-Mizi <67514650+Md-Shihab-Mizi@users.noreply.github.com>
2020-09-25 19:22:42 +0600

    log in page

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBGO4ODBn4nGHDr0u6kqe-UwDH-oCSWVxE",
+    authDomain: "travell-guruu.firebaseapp.com",
+    databaseURL: "https://travell-guruu.firebaseio.com",
+    projectId: "travell-guruu",
+    storageBucket: "travell-guruu.appspot.com",
+    messagingSenderId: "544786223199",
+    appId: "1:544786223199:web:0fd82c6f150d5156450bea"
+  };
+  export default firebaseConfig;
